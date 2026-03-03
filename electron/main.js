bc6d721d07094778eb4e92c97498d31637f75367
JakobOpresnik <jakob.opresnik@gmail.com>
2026-03-03 20:52:06 +0100

    add requirements.txt files and other minor fixes

--- electron/main.js
+++ electron/main.js
@@ -33,8 +33,8 @@ function createWindow() {
     width: 1920,
     height: 1080,
     icon: path.join(__dirname, 'desktop_icon.ico'),
-    autoHideMenuBar: true, // hides menu bar
-    frame: false, // removes title bar
+    autoHideMenuBar: false, // hides menu bar
+    frame: true, // removes title bar
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
